e8c35bb016afadf8e7e4239bafce21bdfd04a988
dinhjeffrey <dinhjeffreyy@gmail.com>
2016-03-27 04:24:51 +0000

    parsed opSystem

--- app/api/headerParser.js
+++ app/api/headerParser.js
@@ -9,9 +9,9 @@ module.exports = function(app) {
         var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
         var lang = req.headers["accept-language"]
         lang = lang.slice(0, lang.indexOf(','))
-        var opSystem = os.platform() + "; " + os.release(); //'10.8.0'
-        var opSystem2 = req.headers['user-agent']
-        var userInfo = { "IP Address": ip, "Language": lang, "Operating System": opSystem, "Operating System2": opSystem2 }
+        var opSystem = req.headers['user-agent']
+        opSystem = opSystem.slice(opSystem.indexOf('(') + 1, opSystem.indexOf(')'))
+        var userInfo = { "IP Address": ip, "Language": lang, "Operating System": opSystem }
             res.send(JSON.stringify(userInfo))
         })
 }
