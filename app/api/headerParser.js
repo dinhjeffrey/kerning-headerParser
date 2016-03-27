0d61e60d100c6736ea95b9c0ae35bbe8ba0058fc
dinhjeffrey <dinhjeffreyy@gmail.com>
2016-03-27 03:54:26 +0000

    added ip lang os

--- app/api/headerParser.js
+++ app/api/headerParser.js
@@ -1,11 +1,14 @@
 'use strict';
 var express = require('express');
+var os = require('os');
 
 
 module.exports = function(app) {
     app.get('/whoami', function (req, res) {
     // need access to IP address here
         var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-        res.send(JSON.stringify(ip))
+        var lang = req.headers["accept-language"]
+        var opSystem = os.platform() + "; " + os.release(); //'10.8.0'
+        res.send(JSON.stringify('IP Address: 'ip + '\n' + 'Language: '+ lang + '\n' + 'Operating System: ' + opSystem))
     })
 }
