998e2e3eb9285750e61aa02a7b76520b31373cef
dinhjeffrey <dinhjeffreyy@gmail.com>
2016-03-27 04:08:03 +0000

    parsed url

--- app/api/headerParser.js
+++ app/api/headerParser.js
@@ -7,7 +7,7 @@ module.exports = function(app) {
     app.get('/whoami', function (req, res) {
     // need access to IP address here
         var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-        var lang = req.headers["accept-language"]
+        var lang = req.acceptedLanguages
         var opSystem = os.platform() + "; " + os.release(); //'10.8.0'
         var userInfo = { "IP Address": ip, "Language": lang, "Operating System": opSystem }
             res.send(JSON.stringify(userInfo))
