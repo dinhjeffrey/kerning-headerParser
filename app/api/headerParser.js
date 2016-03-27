7fd5f9942daf9bb362db3b244fba967d8571af3e
dinhjeffrey <dinhjeffreyy@gmail.com>
2016-03-27 04:22:18 +0000

    added user-agent

--- app/api/headerParser.js
+++ app/api/headerParser.js
@@ -8,15 +8,10 @@ module.exports = function(app) {
     // need access to IP address here
         var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
         var lang = req.headers["accept-language"]
-        var langParseIndexSemi = lang.indexOf(';');
-        var langParseIndexComma = lang.indexOf(',');
-        if (langParseIndexSemi < langParseIndexComma) {
-            lang = lang.slice(0,langParseIndexSemi)
-        } else {
-            lang = lang.slice(0,langParseIndexComma)
-        }
+        lang = lang.slice(0, lang.indexOf(','))
         var opSystem = os.platform() + "; " + os.release(); //'10.8.0'
-        var userInfo = { "IP Address": ip, "Language": lang, "Operating System": opSystem }
+        var opSystem2 = req.headers['user-agent']
+        var userInfo = { "IP Address": ip, "Language": lang, "Operating System": opSystem, "Operating System2": opSystem2 }
             res.send(JSON.stringify(userInfo))
         })
 }
