c5e3b624e1c9cd7b2fc72ed8b3637db40eb0783a
dinhjeffrey <dinhjeffreyy@gmail.com>
2016-03-27 04:13:51 +0000

    fixed lang parser

--- app/api/headerParser.js
+++ app/api/headerParser.js
@@ -7,7 +7,9 @@ module.exports = function(app) {
     app.get('/whoami', function (req, res) {
     // need access to IP address here
         var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-        var lang = req.acceptedLanguages
+        var lang = req.headers["accept-language"]
+        var langParseIndex = lang.indexOf(';');
+        lang = lang.slice(0,langParseIndex)
         var opSystem = os.platform() + "; " + os.release(); //'10.8.0'
         var userInfo = { "IP Address": ip, "Language": lang, "Operating System": opSystem }
             res.send(JSON.stringify(userInfo))
