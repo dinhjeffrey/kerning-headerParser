19a71621d15e8df0881e113ec9c657545308b9fe
dinhjeffrey <dinhjeffreyy@gmail.com>
2016-03-27 03:23:21 +0000

    require express

--- app/api/headerParser.js
+++ app/api/headerParser.js
@@ -1,10 +1,11 @@
 'use strict';
-var os = require('os');
+var express = require('express');
 
 
 module.exports = function(app) {
     app.post('/whoami', function (req, res) {
     // need access to IP address here
         req.headers['x-forwarded-for']
+        console.log(req.headers['x-forwarded-for'])
     })
 }
