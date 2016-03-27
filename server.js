bc75c92efbd1d8c2348609435f1575f62e880a9e
dinhjeffrey <dinhjeffreyy@gmail.com>
2016-03-27 02:56:50 +0000

    remove api bcuz it broke site

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var routes = require('./app/routes/index.js');
 var mongoose = require('mongoose');
 var passport = require('passport');
 var session = require('express-session');
-var api = require('./app/api/headerParser.js')
+// var api = require('./app/api/headerParser.js')
 
 var app = express();
 require('dotenv').load();
