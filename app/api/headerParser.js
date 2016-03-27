bec6ef1f79dae6a94aa8424d1304e489529f810a
dinhjeffrey <dinhjeffreyy@gmail.com>
2016-03-27 03:33:54 +0000

    added req.ip

--- app/api/headerParser.js
+++ app/api/headerParser.js
@@ -6,5 +6,6 @@ module.exports = function(app) {
     app.get('/whoami', function (req, res) {
     // need access to IP address here
         var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+        req.ip
     })
 }
